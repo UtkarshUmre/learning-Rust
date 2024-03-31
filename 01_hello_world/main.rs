5624308806e898e78ebe3b8fc410edf6fb53fa34
utkarsh <utkarshumre@outlook.com>
2024-03-31 08:37:38 +0530

    bunch

--- 01_hello_world/main.rs
+++ 01_hello_world/main.rs
@@ -1,3 +1,17 @@
+use ferris_says::says;
+use std::io::{stdout, BufWriter};
+
+
 fn main(){
-    print!("Hello world!\n")
+    let stdout = stdout();
+    let message = String::from("Hello fellow rustaceans!")
+    let width = message.chars().count();
+
+
+    let mut write = BufWriter::new(stdout.lock());
+    says(message.as_bytes(), width, &mut write).unwrap();
+
+
+
+
 }
