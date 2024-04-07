08aaed951cf52b0eb4d50c4f34e9a23d0dbd5a2a
utkarsh <utkarshumre@outlook.com>
2024-04-07 23:18:30 +0530

    restart

--- first-rust-program/target/debug/deps/first_rust_program-160acea19bcd143a.d
+++
@@ -1,5 +0,0 @@
-/home/utkarshumre/learning-Rust/first-rust-program/target/debug/deps/first_rust_program-160acea19bcd143a: src/main.rs
-
-/home/utkarshumre/learning-Rust/first-rust-program/target/debug/deps/first_rust_program-160acea19bcd143a.d: src/main.rs
-
-src/main.rs:
--- first-rust-program/target/debug/deps/smallvec-b5d1fa59550b5e13.d
+++
@@ -1,7 +0,0 @@
-/home/utkarshumre/learning-Rust/first-rust-program/target/debug/deps/libsmallvec-b5d1fa59550b5e13.rmeta: /home/utkarshumre/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-0.4.5/lib.rs
-
-/home/utkarshumre/learning-Rust/first-rust-program/target/debug/deps/libsmallvec-b5d1fa59550b5e13.rlib: /home/utkarshumre/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-0.4.5/lib.rs
-
-/home/utkarshumre/learning-Rust/first-rust-program/target/debug/deps/smallvec-b5d1fa59550b5e13.d: /home/utkarshumre/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-0.4.5/lib.rs
-
-/home/utkarshumre/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-0.4.5/lib.rs:
